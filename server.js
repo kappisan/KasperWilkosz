b8bea3b251a1bd644517ba80e2bd44bca1bf10eb
Kappisan <wilkosz.ian@gmail.com>
2016-11-20 15:16:10 +0000

    three js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const PORT=9999;
 var express = require('express');
 var app = express();
 
-app.use(express.static('./static/'));
+app.use(express.static('./'));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
